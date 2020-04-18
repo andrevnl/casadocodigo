0121aec02a109c192e02716f04bbb97a3e59e232
andrevnl <andrevnl@hotmail.com>
2020-04-18 00:19:59 -0300

    Madando valor da compra para um metodo de pagamento simulado

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoItem.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoItem.java
@@ -1,9 +1,11 @@
 package br.com.casadocodigo.loja.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class CarrinhoItem {
+public class CarrinhoItem implements Serializable {
+    private static final long serialVersionUID = 6158917804016912279L;
 
     private Produto produto;
     private TipoPreco tipoPreco;
--- src/main/webapp/WEB-INF/views/produtos/lista.jsp
+++ src/main/webapp/WEB-INF/views/produtos/lista.jsp
@@ -11,6 +11,8 @@
 <body>
     <h1>Lista de Produtos</h1>
     <div>${sucesso}</div>
+    <div>${falha}</div>
+
     <table>
         <tr>
             <td>Título</td>
