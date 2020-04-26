9037a9ba61a78ad9ee4823d2e3fa753215ff1e53
andrevnl <andrevnl@hotmail.com>
2020-04-26 17:42:24 -0300

    Melhorando o home

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,15 +1,26 @@
 package br.com.casadocodigo.loja.controllers;
 
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private ProdutoDAO produtoDao;
+
 	@RequestMapping("/")
-	public String index() {
-		System.out.println("Entrando na Home da CDC");
-		return "home";
+	public ModelAndView index() {
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
 	}
 	
 }
