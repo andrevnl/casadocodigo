2787843ad6e8be01d3ceefa7b31348bfce1d9ac5
andrevnl <andrevnl@hotmail.com>
2020-04-26 18:06:57 -0300

    Invalidando cache depois da gravação de um novo livro

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController {
 	private ProdutoDAO produtoDao;
 
 	@RequestMapping("/")
-	@Cacheable(value = "produtoHome")
+	@Cacheable(value = "produtosHome")
 	public ModelAndView index() {
 		List<Produto> produtos = produtoDao.listar();
 		ModelAndView modelAndView = new ModelAndView("home");
--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -4,6 +4,7 @@ import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.TipoPreco;
 import br.com.casadocodigo.loja.validation.ProdutoValidation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -44,6 +45,7 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
+	@CacheEvict(value = "produtosHome", allEntries = true)
 	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
 
 		if (result.hasErrors()) {
