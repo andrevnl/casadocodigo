3efde7a6823d40d9e2590893d21e5268a88d8eb3
andrevnl <andrevnl@hotmail.com>
2020-05-23 14:18:54 -0300

    Tentando trocar o jeito de pegar as informações do banco pra ver se funciona

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -31,7 +31,9 @@ public class JPAProductionConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         // usuario:senha@host:port/path
-        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+//        URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
+        URI dbUrl = new URI(System.getenv("DATABASE_URL"));
+
 
         dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
         dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
